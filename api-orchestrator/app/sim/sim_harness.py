7544f4f30e191ababe7986cc333ae3bc016c8300
Dan Rossiter <dan@rossiters.org>
2026-02-21 21:14:50 +0000

    Implementing initial sim harness to run provided `sim.py`. Tested REST->DB/Queue->DB->REST successfully.

---
+++ api-orchestrator/app/sim/sim_harness.py
@@ -0,0 +1,37 @@
+import logging
+import json
+import uuid
+from datetime import datetime
+import tempfile
+from app.sim.bowling.sim import BowlingSimulation
+from app.schemas.bowling_config import BowlingConfig
+
+def run_simulation(logger: logging.Logger, id: uuid.UUID, velocity: float, rpm: int, friction: float, launch_angle: float, lateral_offset: float) -> None:
+  # collect baseline codefied as the example JSON for our BowlingConfig model
+  # this is the identical sample JSON originally shipped with sim.py
+  json_dict = BowlingConfig.model_json_schema()['example']
+  
+  # tweak baseline, injecting the values different from example JSON
+  json_dict['simulation_metadata']['operator'] = "dan-simulation-driver"
+  
+  # per run variables
+  json_dict['simulation_metadata']['scenario_id'] = f"sim-{id}"
+  json_dict['simulation_metadata']['timestamp'] = datetime.now().isoformat()
+  json_dict['ball_parameters']['initial_velocity_ms'] = velocity
+  json_dict['ball_parameters']['rotational_speed_rpm'] = rpm
+  json_dict['ball_parameters']['launch_angle_deg'] = launch_angle
+  json_dict['lane_conditions']['friction_coefficient'] = friction
+  json_dict['release_point']['lateral_offset_m'] = lateral_offset
+  
+  json_str: str = json.dumps(json_dict, indent=2)
+  
+  logger.debug(f"Running simulation with config:\n{json_str}")
+  
+  with tempfile.NamedTemporaryFile(mode='w+t') as tf:
+    tf.write(json_str)
+    tf.flush()
+    
+    sim = BowlingSimulation(tf.name)
+  
+  sim_result: Dict = sim.run()
+  return sim_result
--- api-orchestrator/app/sim_main.py
+++ api-orchestrator/app/sim_main.py
@@ -11,6 +11,7 @@ from app.db import models
 from app.db.session import DbSession
 from app.queue.session import RedisSession
 from app.service.simulation_service import SimulationService
+from app.sim.sim_harness import run_simulation
 
 if __name__ == "__main__":
   configure_logging()
@@ -23,6 +24,7 @@ if __name__ == "__main__":
   
   # TODO - consider using a more robust worker framework like Celery or RQ for better performance and reliability in production environment
   while True:
+    # TODO - DB flush/commit/close logic below is a mess. Fix it.
     try:
       # block until a new simulation ID is available in the queue
       item: tuple[str, str] | None = cast(tuple[str, str] | None, redis.blpop([settings.queue_name], timeout=30)) # type: ignore
@@ -52,12 +54,47 @@ if __name__ == "__main__":
       sim.started_at = datetime.now()
       db.flush()
       
-      # TODO - add actual simulation processing logic here, e.g. call a physics engine with the simulation parameters from the database, store results back in the database, etc.
-      
+      # run the simulation
+      try:
+        logger.debug(f"Running simulation with ID: {sim_id}")
+        sim_result = run_simulation(
+          logger, uuid.UUID(sim_id),
+          sim.velocity, sim.rpm, sim.friction, sim.launch_angle, sim.lateral_offset)
+        logger.debug(f"Simulation with ID: {sim_id} completed")
+      except Exception as e:
+        logger.error(f"Error running simulation with ID: {sim_id}. Error: {e}")
+        sim.status = models.SimulationState.FAILED
+        sim.error_msg = str(e)
+        sim.completed_at = datetime.now()
+        db.commit()
+        continue
+            
       sim.status = models.SimulationState.COMPLETED
       sim.completed_at = datetime.now()
+      
+      # NOTE: Sim inconsistently returns some values as numpy floats and others as native float values
+      # TO be safe and future-proof, casting all number values to native type explicitly before passing to DB
+      sim.results = models.SimulationResult(
+        simulation_id=sim.id,
+        execution_duration=float(sim_result['simulation_metadata']['execution_duration_ms']),
+        pins_knocked=int(sim_result['simulation_results']['pins_knocked']),
+        impact_velocity=float(sim_result['trajectory_analysis']['impact_velocity_ms']),
+        hook_potential=float(sim_result['trajectory_analysis']['hook_potential_cm']),
+        entry_angle=float(sim_result['trajectory_analysis']['entry_angle_deg']),
+        ball_deflection=float(sim_result['trajectory_analysis']['ball_deflection_cm']),
+        trajectory_length=float(sim_result['trajectory_analysis']['trajectory_length_m']),
+        pin_states=[
+          models.PinState(
+            pin_number=int(pin['pin_number']),
+            knocked=bool(pin['knocked']),
+            impact_velocity=float(pin['impact_velocity_ms']) if pin.get('impact_velocity_ms') is not None else None
+          ) for pin in sim_result['simulation_results']['pin_states']
+        ]
+      )
+      
       db.commit()
       logger.info(f"Completed simulation with ID: {sim_id}")
+      logger.info(f"execution_duration_ms: {sim_result['simulation_metadata']['execution_duration_ms']}, pins_knocked: {sim_result['simulation_results']['pins_knocked']}, impact_velocity_ms: {sim_result['trajectory_analysis']['impact_velocity_ms']}, hook_potential_cm: {sim_result['trajectory_analysis']['hook_potential_cm']}, entry_angle_deg: {sim_result['trajectory_analysis']['entry_angle_deg']}, ball_deflection_cm: {sim_result['trajectory_analysis']['ball_deflection_cm']}, trajectory_length_m: {sim_result['trajectory_analysis']['trajectory_length_m']}")
       continue
     except Exception as e:
       logger.error(f"Error processing simulation with ID: {sim_id}. Error: {e}")
